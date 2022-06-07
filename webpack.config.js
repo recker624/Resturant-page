ac55c6371f23d522e882226932bd38092bf863d6
recker624 <koolcephoid@gmail.com>
2022-06-07 19:03:45 +0530

    Started work on homepage

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,13 @@ module.exports = {
   devServer: {
     static: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+    ],
+  },
   mode: 'development',
 };
