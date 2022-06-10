e27c7f98c7ac09f542ccfa8465c5ff69bf69ac6f
recker624 <koolcephoid@gmail.com>
2022-06-10 13:14:29 +0530

    Minor updates to front page

---
+++ src/page_1.js
@@ -0,0 +1,15 @@
+//implement all other functionality in helper functions
+
+//minimize use of global variables!
+
+"use strict"
+
+let loadPage = (function() {
+
+
+   return {
+
+   };
+}());
+
+export {loadPage};
